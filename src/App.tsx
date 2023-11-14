b9809d8c0cd5468e857b2eeefe16452593ed5ff6
cristianvsirbu <cristiansirbu4@gmail.com>
2023-11-14 11:54:00 +0200

    Initiated default structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
+import Hero from './components/Hero'
+import About from './components/About'
+import Projects from './components/Projects'
+import CTA from './components/CTA'
 
 function App() {
-  (
     <div className='app'>
-      
+      <Hero />
+      <About />
+      <Projects />
+      <CTA />
     </div>
-  )
 }
 
 export default App
