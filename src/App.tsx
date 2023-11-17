11da8e17b8e056169be894d2ef6e25892b5d7855
cristianvsirbu <cristiansirbu4@gmail.com>
2023-11-17 19:20:02 +0200

    Styled the Hero component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import './App.css'
-import Hero from './components/Hero'
-import About from './components/About'
-import Projects from './components/Projects'
-import CTA from './components/CTA'
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import CTA from "./components/CTA";
 
 const App = () => {
-  return (
-    <div className=''>
-      <Hero />
-      <About />
-      <Projects />
-      <CTA />
-    </div>
-  )
-}
+	return (
+		<div className="">
+			<Hero />
+			<About />
+			<Projects />
+			<CTA />
+		</div>
+	);
+};
 
-export default App
+export default App;
