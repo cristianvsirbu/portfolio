19c4d027425fb28ce84c0b015f31f7a57a3cb49b
cristianvsirbu <cristiansirbu4@gmail.com>
2023-11-17 17:28:11 +0200

    removed old tailwind code, added assets

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,22 @@
-
 const Hero = () => {
-  return (
-    <div className="">
-      <h1>Hero</h1>
-    </div>
-  )
-}
+	return (
+		<div className="flex flex-col items-center justify-center space-y-10">
+			<div className="w-[25rem] h-[25rem] bg-slate-400 rounded-full flex items-center justify-center">
+				<img
+					src="/profile.png"
+					alt="Profile Picture"
+					className="w-[90%] h-[90%]"
+				/>
+			</div>
+			<div className="w-full h-full font-bold text-7xl text-center text-white">
+				<p className="">
+					Hi, I'm <span className="my_name">Cristian</span>. <br /> A Web
+					Developer.
+				</p>
+			</div>
+			<img src="/public/hero-vector.svg" alt="Hero Vector" className="" />
+		</div>
+	);
+};
 
-export default Hero
+export default Hero;
