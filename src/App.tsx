0dbb06c0290ee225d7cf2cd1475cc363e4d27a59
cristianvsirbu <cristiansirbu4@gmail.com>
2023-11-21 19:33:17 +0200

    Added arrow for Projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import Hero from "./components/Hero";
 import About from "./components/About";
-import Projects from "./components/Projects";
-import CTA from "./components/CTA";
 
 const App = () => {
 	return (
 		<div className="top_level_container">
 			<Hero />
 			<About />
-			<Projects />
-			<CTA />
 		</div>
 	);
 };
