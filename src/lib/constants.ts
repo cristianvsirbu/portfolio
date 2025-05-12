0dd995f658820cf1ecb9697d955f455280c8671f
Cristian Sîrbu <cristiansirbu4@gmail.com>
2025-05-12 16:18:58 +0300

    feat: remove unused SVG files and add constants and utility functions

---
+++ src/lib/constants.ts
@@ -0,0 +1,9 @@
+import { getCurrentYear } from './utils';
+
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/cristianvsirbu/';
+export const GITHUB_URL = 'https://github.com/cristianvsirbu';
+export const PHONE_NUMBER = '+40 755 307 722';
+export const EMAIL = 'cristiansirbudev@gmail.com';
+export const LOCATION = 'Bucharest, Romania';
+export const PROGRAMMING_YEARS = getCurrentYear() - 2018;
+export const PROFESSIONAL_YEARS = getCurrentYear() - 2021;
