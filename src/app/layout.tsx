11f15674999be2f5d0f0be1e339084c08f31fa25
Cristian Sîrbu <cristiansirbu4@gmail.com>
2025-05-12 16:50:12 +0300

    chore: basic project structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import Header from '@/components/Header';
 
 
 export const metadata: Metadata = {
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body
       >
+        <Header />
         {children}
       </body>
     </html>
---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Header = () => {
+    const navLinks = [
+        { href: '/', label: 'Home' },
+        { href: '/about', label: 'About' },
+        { href: '/experience', label: 'Experience' },
+        { href: '/projects', label: 'Projects' },
+        { href: '/contact', label: 'Contact' },
+      ];
+    return (
+        <header className="flex justify-center items-center p-4 bg-gray-800 text-white">
+            <nav>
+                <ul className="flex space-x-4">
+                    {navLinks.map((link) => (
+                        <li key={link.href}>
+                            <Link href={link.href} className="hover:text-gray-400">
+                                {link.label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
