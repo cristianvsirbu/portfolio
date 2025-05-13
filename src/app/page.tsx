80813715bd35ba9af48a45ab3c8f1ccec5dd8f49
Cristian Sîrbu <cristiansirbu4@gmail.com>
2025-05-13 13:29:08 +0300

    feat: update home page layout and content for portfolio website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <h1 className="text-2xl font-bold">Hello World!</h1>;
+  return (
+    <main className="bg-black">
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl text-white">Cristian Sîrbu</h1>
+        <p className="text-gray-400 mt-4">Welcome to my portfolio website!</p>
+      </div>
+    </main>
+  );
 }
