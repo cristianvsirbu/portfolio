dd588e3bf763c9f2f51e0af7f20f1db02e94b7f4
Cristian Sîrbu <cristiansirbu4@gmail.com>
2025-05-19 17:28:19 +0300

    chore: remove PHONE_NUMBER constant from constants file

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,6 @@ import { getCurrentYear } from './utils';
 
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/cristianvsirbu/';
 export const GITHUB_URL = 'https://github.com/cristianvsirbu';
-export const PHONE_NUMBER = '+40 755 307 722';
 export const EMAIL = 'cristiansirbudev@gmail.com';
 export const LOCATION = 'Bucharest, Romania';
 export const PROGRAMMING_YEARS = getCurrentYear() - 2018;
