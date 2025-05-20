b986508a39b58febfa7c3d4a007e70414357f4c6
Cristian Sîrbu <cristiansirbu4@gmail.com>
2025-05-20 17:01:37 +0300

    feat: added all technologies icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,13 +76,13 @@ export default function Home() {
 
       {activeSection === 'home' && (
         <div className="flex flex-col flex-grow items-center justify-center w-full h-full">
-          <h1 className="text-7xl leading-tight text-white font-medium">
+          <h1 className="text-7xl leading-none text-white font-medium">
             Hi, I’m Cristian Sîrbu
           </h1>
-          <h1 className="text-6xl text-white leading-none font-medium">
+          <h1 className="text-6xl text-white leading-tight font-medium">
             I bring pixels to life with code
           </h1>
-          <p className="mt-4 text-xl max-w-[1000px] text-center">
+          <p className="text-xl max-w-[1000px] text-[#86868B] text-center">
             Frontend dev who crafts seamless web experiences with React, clean
             code, and a touch of creative magic. I obsess over the details -
             from buttery animations to maintainable code, because great
