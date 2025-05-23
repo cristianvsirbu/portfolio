4cfeda32523d774385cfda7c4345679d8f0a6d9b
Cristian Sîrbu <cristiansirbu4@gmail.com>
2025-05-23 21:15:22 +0300

    fix: replace section tags with fragments in About, Contact, Experience, and Projects components

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,7 +4,7 @@ import { PROJECTS } from '@/lib/constants';
 
 const Projects = () => {
   return (
-    <section>
+    <>
       <h1 className="title">Projects</h1>
       <h2 className="subtitle">Stuff I&apos;ve Made:</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-16 mt-8">
@@ -12,7 +12,7 @@ const Projects = () => {
           <Project key={project.name} project={project} />
         ))}
       </div>
-    </section>
+    </>
   );
 };
 
